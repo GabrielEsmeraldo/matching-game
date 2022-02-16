25230613091a2f48f8212fc60195d72d783b599f
gabriel <gabriel-esmeraldo@hotmail.com>
2022-02-16 05:50:30 -0300

    create CardGame component

---
+++ main.js
@@ -0,0 +1,9 @@
+import CardGame from "./src/components/CardGame";
+import './src/styles/generic/reset.css'
+import './src/styles/settings/colors.css'
+import './src/styles/elements/base.css'
+
+const $root = document.querySelector('#root')
+const $htmlCardGame = CardGame()
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame)
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame(){
+    return `
+        <article id='card-game'>
+            <img src='src/components/CardGame/img/alura-pixel.png' id='logo-alura'>
+        </article>
+    `
+}
+
+export default CardGame 
