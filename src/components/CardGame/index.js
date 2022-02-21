e598a7a7c52a30851201c59860b1d9fc6a070964
gabriel <gabriel-esmeraldo@hotmail.com>
2022-02-21 05:21:51 -0300

    Create CardFrontBack animation

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,7 +2,7 @@ import './style.css'
 
 function CardGame(img = 'alura-pixel', alt = 'Logo Alura'){
     return /*html*/ `
-        <article id='card-game'>
+        <article class='card-game'>
             <img src='src/components/CardGame/img/${img}.png' id='${alt}'>
         </article>
     `
