96b7a52e5ec01fb7b153e820d68272d69ee364d2
gabriel <gabriel-esmeraldo@hotmail.com>
2022-02-22 12:32:10 -0300

    Create vsPlayer component

---
+++ src/components/vsPlayer/index.js
@@ -0,0 +1,7 @@
+function vsPlayer(content = 'vs'){
+    return /*html*/ `
+        <span class="vs-player">${content}</span>
+    `
+}
+
+export default vsPlayer
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from "../../components/PlayerName"
+import vsPlayer from "../../components/vsPlayer"
 import '../ScoreBoard/style.css'
 
 function ScoreBoard(){
     return /*html*/`
         <header class='score-board'>
             ${PlayerName('Player 1')}
+            ${vsPlayer()}
             ${PlayerName('Player 2')}
         </header>
     `
